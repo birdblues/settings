67467f469b234c6bb903198a5696b02fda5cd9d2
dhseo <birdblues@mac.com>
2018-02-18 13:17:09 +0900

    Update settings.sh

--- setttings_ubuntu.sh
+++ setttings_ubuntu.sh
@@ -15,5 +15,24 @@ sudo apt install vim
 #sudo apt remove vim
 #sudo add-apt-repository --remove ppa:jonathonf/vim
 
+# install powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+curl -sLf https://spacevim.org/install.sh | bash
+
 #install vim vundle 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone git://github.com/tpope/vim-unimpaired.git ~/.vim/bundle/vim-unimpaired
+
+# install YouCompleteMe
+#sudo apt-get install build-essential cmake
+sudo apt-get install build-essential cmake3
+sudo apt-get install python-dev python3-dev
+sudo apt-get install npm
+
+npm install -g typescript
+
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer --js-completer --java-completer
+
+
